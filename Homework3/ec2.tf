243571d1baff40cae38985a66189adbd4aae63b4
BibigulKosherova <bibigulkosherova@gmail.com>
2025-04-09 02:26:28 +0000

    added corrected files

--- Homework3/ec2.tf
+++ Homework3/ec2.tf
@@ -1,5 +1,19 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "ubuntu" {
-  ami                    = "ami-075686beab831bb7f"
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public1.id
   vpc_security_group_ids = [aws_security_group.allow_common_ports.id]
@@ -10,8 +24,26 @@ resource "aws_instance" "ubuntu" {
   }
 }
 
+
+data "aws_ami" "amazon" {
+  most_recent = true
+  owners      = ["137112412989"] 
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+
 resource "aws_instance" "amazon" {
-  ami                    = "ami-087f352c165340ea1"
+  ami                    = data.aws_ami.amazon.id
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public2.id
   vpc_security_group_ids = [aws_security_group.allow_common_ports.id]
